ac2495cb3f66a05a5ce828aa2889068fd62b9851
Simon Nishi McCorkindale <simon@funx.jp>
2020-08-24 23:24:38 +0900

    Initialize Google Maps for iOS with API KEY

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // For google_maps_flutter
+    GMSServices.provideAPIKey("AIzaSyAOnd_RLDnvFHjA9PXdYjMSNvE8ALp7ly0")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
