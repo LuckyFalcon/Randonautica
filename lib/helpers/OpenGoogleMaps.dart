378e031b367d35d6269d0a6e65fac6f0a3dda0c8
Simon Nishi McCorkindale <simon@funx.jp>
2020-09-10 08:56:12 +0900

    Open Google Maps app on iOS, else failback to Apple Maps

--- lib/helpers/OpenGoogleMaps.dart
+++ lib/helpers/OpenGoogleMaps.dart
@@ -5,11 +5,13 @@ class MapUtils {
   static Future<void> openMap(double latitude, double longitude) async {
 
     String googleUrl = "https://www.google.com/maps/place/" + latitude.toString() + "+" + longitude.toString() + "/@" + latitude.toString() + "+" + longitude.toString() + ",14z&zoom=14&mapmode=standard";
+    googleUrl = googleUrl.replaceAll("https://", "comgooglemaps://");
 
     if (await canLaunch(googleUrl)) {
       await launch(googleUrl);
     } else {
-      throw 'Could not open the map.';
-    }
+      // Fall back to Apple Maps
+      String coords = latitude.toString() + "," + longitude.toString();
+      await launch("http://maps.apple.com/?daddr=" + coords);    }
   }
 }
